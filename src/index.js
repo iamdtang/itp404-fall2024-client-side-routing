69b85f0a129aa133721548ef32b78922884f8af4
David Tang <687449+iamdtang@users.noreply.github.com>
2024-10-14 11:54:35 -0700

    Index and Contact routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Index from "./routes/Index";
+import Contact from "./routes/Contact";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "bootstrap/dist/css/bootstrap.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/routes/Contact.jsx
@@ -0,0 +1,10 @@
+import Navigation from "../Navigation";
+
+export default function Contact() {
+  return (
+    <div className="container">
+      <Navigation />
+      <h1>Contact</h1>
+    </div>
+  );
+}
