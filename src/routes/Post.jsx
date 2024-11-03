a8f2452b564780dff18034a3874fee03156bd461
David Tang <687449+iamdtang@users.noreply.github.com>
2024-11-02 22:14:50 -0700

    Deleting a post

--- src/routes/Post.jsx
+++ src/routes/Post.jsx
@@ -1,7 +1,8 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 export default function Post() {
   const post = useLoaderData();
+  const navigate = useNavigate();
 
   return (
     <div className="post-page">
@@ -16,6 +17,26 @@ export default function Post() {
           return <li key={comment.id}>{comment.body}</li>;
         })}
       </ol>
+
+      <button
+        type="button"
+        className="btn btn-danger"
+        onClick={() => {
+          const isDeleteConfirmed = window.confirm(
+            "Are you sure you want to delete this post?"
+          );
+
+          if (isDeleteConfirmed) {
+            fetch(`/posts/${post.id}`, {
+              method: "DELETE",
+            }).then(() => {
+              navigate("/");
+            });
+          }
+        }}
+      >
+        Delete
+      </button>
     </div>
   );
 }
